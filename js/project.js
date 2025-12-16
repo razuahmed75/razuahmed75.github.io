9bb35d9068c7bcaca3a6d988fcaabbceeaf8e390
Razu ahmed <razuahmed8641@gmail.com>
2025-12-17 00:44:20 +0600

    testing project hover

---
+++ js/project.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.hover-img').forEach(img => {
+    const originalSrc = img.src;
+    const hoverSrc = img.dataset.hover;
+    const originalWidth = img.style.width;
+    const originalHeight = img.style.height;
+
+    img.addEventListener('mouseenter', () => {
+        img.src = hoverSrc;
+        img.style.width = '100%';
+        img.style.height = '280px';
+    });
+
+    img.addEventListener('mouseleave', () => {
+        img.src = originalSrc;
+        img.style.width = originalWidth;
+        img.style.height = originalHeight;
+    });
+});
